17a8f676f4485b2e0f189ca6252832dae2540c70
Giovanni Liboni <giovanni.liboni@live.com>
2014-05-08 15:24:40 +0200

    New classes for NodeComputation

---
+++ specializzazioni/Divide.java
@@ -0,0 +1,13 @@
+package specializzazioni;
+
+import nodi.NodeComputation;
+
+public class Divide implements NodeComputation {
+
+	@Override
+	public String operation(float nodeData, float threshold) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
