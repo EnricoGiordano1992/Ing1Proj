8822e3cd7b97394fd7370f40181a2d47542bf798
Giovanni Liboni <giovanni.liboni@live.com>
2014-05-09 20:18:36 +0200

    Sistemati conflitti

--- node/Monitor.java
+++ node/Monitor.java
@@ -6,28 +6,15 @@ import nodeComunication.RxNode;
 public class Monitor extends Node {
 
 	Grafica gg;
-<<<<<<< HEAD
 	private int pos;
-=======
-	int myGID;
->>>>>>> fce21cb68c8b30607c0b0dfdbb504836765df8d7
 	
 	public Monitor(String nodeName, boolean W) {
 		super(nodeName, W);
 		this.nodeComm = new RxNode(this);
-		this.myGID = 0;
 	}
 
 	
 	public void newDisplay(Grafica g)
-=======
-	public void setMyGID(int GID) {
-		
-		this.myGID = GID;
-	}
-	
-	public void setGG(Grafica g)
->>>>>>> fce21cb68c8b30607c0b0dfdbb504836765df8d7
 	{
 		this.gg = g;
 	}
@@ -39,13 +26,7 @@ public class Monitor extends Node {
 	 * Mostra i dati ricevuti attraverso l'oggetto nodeComm
 	 */
 	public void display() {
-<<<<<<< HEAD
 		gg.setLabel(""+ getValue(), pos);
-=======
-		
-			Simulator.g.writeText(myGID-1, getName());
-			Simulator.g.writeText(myGID, "" + (int) getValue());
->>>>>>> fce21cb68c8b30607c0b0dfdbb504836765df8d7
 	}
 
 	// Metodi non usati
--- start/Simulator.java
+++ start/Simulator.java
@@ -1,8 +1,9 @@
 package start;
 
+import grafica.Grafica;
+
 import java.awt.Color;
 
-import grafica.Grafica;
 import node.Detector;
 import node.Monitor;
 import node.Processor;
@@ -28,7 +29,7 @@ public class Simulator {
 		wl.addNode(monitor_ch);
 		wl.addNode(detector);
 		wl.addNode(processor);
-<<<<<<< HEAD
+
 		wl.addNode(monitor_count);
 		
 		g.newDisplay(monitor);
@@ -36,15 +37,6 @@ public class Simulator {
 		g.newDisplay(monitor_ch);
 							
 		for ( int i = 0;; i++ )
-=======
-		
-		
-		monitor.setMyGID(g.newDisplay(Color.blue, Color.white));
-		monitor_ch.setMyGID(g.newDisplay(Color.red, Color.white));
-		
-		
-		for ( int i = 0; i < 800; i++ )
->>>>>>> fce21cb68c8b30607c0b0dfdbb504836765df8d7
 		{
 			try{
 					int randomNum = (int)(Math.random()*500);
